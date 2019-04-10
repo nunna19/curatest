6638b1ec08bb55dda124f79e5ce7bf5c33e0aa5f
nunna19 <nunnapat_ch@yahoo.com>
2019-04-10 14:51:07 -0400

    Updates

---
+++ precache-manifest.dc414292b6e2aad4ce220a7ee3e4bcab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c2631ffb928f610f262",
+    "url": "/curatest/static/css/main.34c5985d.chunk.css"
+  },
+  {
+    "revision": "6c2631ffb928f610f262",
+    "url": "/curatest/static/js/main.a3fc526d.chunk.js"
+  },
+  {
+    "revision": "1c5a2be44baa5eca7c37",
+    "url": "/curatest/static/js/runtime~main.c080bff6.js"
+  },
+  {
+    "revision": "006e21f63032122e69ae",
+    "url": "/curatest/static/js/2.564858ac.chunk.js"
+  },
+  {
+    "revision": "eff0ab0f903a65def809742e13aa04b1",
+    "url": "/curatest/index.html"
+  }
+];
